b99457a132ff75e1cf9b75fb6085cd059afbfbd2
Felipe Duaibs <felipe.duaibs@hotmail.com>
2022-02-23 13:06:53 -0300

    feat: env vars, mongo e crud

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,18 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { ConfigModule } from '@nestjs/config';
+import { UsersModule } from './users/users.module';
+import { MongooseModule } from '@nestjs/mongoose';
 
 @Module({
-  imports: [],
+  imports: [
+    UsersModule,
+    ConfigModule.forRoot(),
+    MongooseModule.forRoot(
+      `mongodb+srv://${process.env.DB_USERNAME}:${process.env.DB_PASSWORD}@estudos.jr7op.mongodb.net/test`,
+    ),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
